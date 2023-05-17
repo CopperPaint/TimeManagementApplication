cfd6ce86433ba6c752b93f731f734110e4cce8b6
jake1970 <jakeyjyoung@gmail.com>
2023-05-17 16:59:52 +0200

    Added Early Entry Method Functionality

--- app/src/main/java/com/example/opsc_poe/MainActivity.kt
+++ app/src/main/java/com/example/opsc_poe/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.opsc_poe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.view.marginLeft
 
 class MainActivity : AppCompatActivity()
 {
@@ -21,5 +24,24 @@ class MainActivity : AppCompatActivity()
         //testing (please) - jake
 
         //testing work - ben
+
+        val signInSelect = findViewById<TextView>(R.id.tvSignIn)
+        val signUpSelect = findViewById<TextView>(R.id.tvSignUp)
+        val EntryMethodSelector = findViewById<View>(R.id.vwEntryMethod)
+
+        signUpSelect.setOnClickListener{
+
+            EntryMethodSelector.left = 500
+            //move or hide the bar to be on the sign up
+
+        }
+
+
+        signInSelect.setOnClickListener{
+
+            EntryMethodSelector.left = 100
+          //move or hide the bar to be on the sign in
+        }
+
     }
 }
