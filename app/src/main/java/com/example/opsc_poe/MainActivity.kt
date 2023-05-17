c7f23bb4fe1c50ae38ed605f7048d0e877d87603
CopperPaint <jsaul1250@gmail.com>
2023-05-17 13:08:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opsc_poe/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.opsc_poe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //PLEASE FUCKING WORK
+    }
+}
