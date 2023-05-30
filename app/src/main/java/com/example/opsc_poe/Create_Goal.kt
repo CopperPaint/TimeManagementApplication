20b28de4f56f19653a04815c4db3a52d2a4c4bef
jake1970 <jakeyjyoung@gmail.com>
2023-05-30 11:58:12 +0200

    Added Goal Scroll Pickers

--- app/src/main/java/com/example/opsc_poe/Create_Goal.kt
+++ app/src/main/java/com/example/opsc_poe/Create_Goal.kt
@@ -1,19 +1,62 @@
 package com.example.opsc_poe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.NumberPicker.OnValueChangeListener
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.opsc_poe.databinding.ActivityCreateGoalBinding
+
 
 class Create_Goal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_goal)
+        //setContentView(R.layout.activity_create_goal)
+
+        val binding = ActivityCreateGoalBinding.inflate(layoutInflater)
+       setContentView(binding.root)
 
         //Hide the action bar
         supportActionBar?.hide()
 
         //set status bar color
         window.statusBarColor = ContextCompat.getColor(this, R.color.Dark_Green)
+
+
+        binding.npHourGoal.minValue = 1
+        binding.npHourGoal.maxValue = 24
+        binding.npHourGoal.descendantFocusability = ViewGroup.FOCUS_BLOCK_DESCENDANTS
+
+        val timeFrame = arrayOf<String>("Daily", "Weekly", "Monthly")
+        binding.npTimeFrameGoal.minValue = 1
+        binding.npTimeFrameGoal.maxValue = 3
+        binding.npTimeFrameGoal.displayedValues = timeFrame
+        binding.npTimeFrameGoal.descendantFocusability = ViewGroup.FOCUS_BLOCK_DESCENDANTS
+
+
+
+        binding.npTimeFrameGoal.setOnValueChangedListener { picker, oldVal, newVal ->
+
+            when (newVal) {
+                1 -> {binding.npHourGoal.minValue = 1
+                binding.npHourGoal.maxValue = 24}
+                2 -> {binding.npHourGoal.minValue = 1
+                    binding.npHourGoal.maxValue = 25}
+                3 -> {binding.npHourGoal.minValue = 1
+                    binding.npHourGoal.maxValue = 26}
+                else -> { // Note the block
+                    binding.npHourGoal.minValue = 1
+                        binding.npHourGoal.maxValue = 24
+                }
+            }
+
+
+        }
+
+
+
+
+
 
     }
 }
