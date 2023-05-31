d3e3707f577595e68727bb8893a7807472596c5c
CopperPaint <jsaul1250@gmail.com>
2023-05-31 17:41:18 +0200

    Custom Componet Update

--- app/src/main/java/com/example/opsc_poe/CustomActivity.kt
+++ app/src/main/java/com/example/opsc_poe/CustomActivity.kt
@@ -2,10 +2,12 @@ package com.example.opsc_poe
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class CustomActivity(
     context: Context,
@@ -16,20 +18,20 @@ class CustomActivity(
     {
         inflate(context, R.layout.customactivity, this)
 
-        val customStyle =  context.obtainStyledAttributes(attrs, R.styleable.customactivitystyle)
+        val customStyle =  context.obtainStyledAttributes(attrs, R.styleable.customactivitystyle, 0 ,0)
 
         val primarytext = findViewById<TextView>(R.id.tvPrimaryText)
         val secondaytext = findViewById<TextView>(R.id.tvSecondaryText)
         //val bar = findViewById<View>(R.id.vBar)
-        //val block = findViewById<View>(R.id.vBlock)
+        val block = findViewById<View>(R.id.vBlock)
         val blocktext = findViewById<TextView>(R.id.tvBlockText)
         val blockx = findViewById<TextView>(R.id.tvBlockX)
+
         try
         {
             primarytext.text = customStyle.getString(R.styleable.customactivitystyle_primarytext)
             secondaytext.text = customStyle.getString(R.styleable.customactivitystyle_secondarytext)
-            //bar.backgroundTintList = ColorStateList.valueOf(R.styleable.customactivitystyle_barcolour)
-            //block.backgroundTintList = ColorStateList.valueOf(R.styleable.customactivity_blockcolour)
+            block.setBackgroundColor(Color.argb(255,90,255,200))
             blocktext.text = customStyle.getString(R.styleable.customactivitystyle_blocktext)
             blockx.text = customStyle.getString(R.styleable.customactivitystyle_xtext)
         }
