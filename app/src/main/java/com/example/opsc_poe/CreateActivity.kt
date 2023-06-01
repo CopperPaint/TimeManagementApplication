547d0385a9e605d0bfdfba786882e4a6670871b5
bbninja1 <shirk.benjamin@gmail.com>
2023-06-01 11:00:07 +0200

    Update

--- app/src/main/java/com/example/opsc_poe/CreateActivity.kt
+++ app/src/main/java/com/example/opsc_poe/CreateActivity.kt
@@ -2,10 +2,24 @@ package com.example.opsc_poe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.opsc_poe.databinding.ActivityCreateBinding
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CreateActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
     }
+
+
+
 }
