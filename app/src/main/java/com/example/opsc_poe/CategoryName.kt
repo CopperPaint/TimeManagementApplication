21c22205263052819e9ba19f861d21a036143a77
CopperPaint <jsaul1250@gmail.com>
2023-06-01 16:44:08 +0200

    view category update

--- app/src/main/java/com/example/opsc_poe/CategoryName.kt
+++ app/src/main/java/com/example/opsc_poe/CategoryName.kt
@@ -1,11 +1,55 @@
 package com.example.opsc_poe
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class CategoryName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_name)
+
+
+
+        val CategoryName = findViewById<TextView>(R.id.tvCategoryName)
+        CategoryName.text = "set cat name"
+
+        val CategoryDescription = findViewById<TextView>(R.id.txtCategoryDescription)
+        CategoryDescription.text = "Insert cat description"
+
+
+
+        val activityLayout = findViewById<LinearLayout>(R.id.llactivitycontainer)
+
+        //for each activity in category
+        val customactivity = CustomActivity(this)
+
+        //set activity name
+        val PrimaryText = customactivity.findViewById<TextView>(R.id.tvPrimaryText)
+        PrimaryText.text = "set activity name"
+
+        //leave secondary blank
+        val SecondayText = customactivity.findViewById<TextView>(R.id.tvSecondaryText)
+        SecondayText.text = ""
+
+        //display hours until goal
+        val Hour = customactivity.findViewById<TextView>(R.id.tvBlockX)
+        Hour.text = "insert amount of hours until goal"
+        //if goal reached
+        Hour.text = "✔"
+
+        //set block colour to category colour
+        val blockcolour = customactivity.findViewById<TextView>(R.id.vBlock)
+        //leave alpha as 255
+        blockcolour.setBackgroundColor(Color.argb(255,90,255,200))
+
+        //add to view
+        activityLayout.addView(customactivity)
+
+
+
+
     }
 }
