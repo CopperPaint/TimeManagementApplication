bc96def31d19939d5c212a4971fcafa6e0910434
CopperPaint <jsaul1250@gmail.com>
2023-06-01 19:53:04 +0200

    dataclass update

--- app/src/main/java/com/example/opsc_poe/Temp_ActivityDataClass.kt
+++ app/src/main/java/com/example/opsc_poe/Temp_ActivityDataClass.kt
@@ -1,9 +1,9 @@
 package com.example.opsc_poe
 
 data class Temp_ActivityDataClass(
-    var activityID: String = "",
-    var userID: String = "",
-    var categoryID: String = "",
+    var activityID: Int = 0,
+    var userID: Int = 0,
+    var categoryID: Int = 0,
     var name: String = "",
     var description: String = "",
     var maxgoal: String = "",
--- app/src/main/java/com/example/opsc_poe/Temp_LogDataClass.kt
+++ app/src/main/java/com/example/opsc_poe/Temp_LogDataClass.kt
@@ -3,9 +3,9 @@ package com.example.opsc_poe
 import java.util.Date
 
 data class Temp_LogDataClass(
-    var logID: String = "",
-    var activityID: String = "",
-    var userID: String = "",
+    var logID: Int = 0,
+    var activityID: Int = 0,
+    var userID: Int = 0,
     var startDate: Date,
     var endDate: Date,
     var hours: Int
