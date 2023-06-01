543ff732c76275b25cfe519c3bd7637b64a95cd0
CopperPaint <jsaul1250@gmail.com>
2023-06-01 10:53:09 +0200

    Activity view update

--- app/src/main/java/com/example/opsc_poe/ViewActivity.kt
+++ app/src/main/java/com/example/opsc_poe/ViewActivity.kt
@@ -2,10 +2,25 @@ package com.example.opsc_poe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class ViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
+
+        val goalContainer = findViewById<LinearLayout>(R.id.llgoalcontainer)
+
+        val customMaxGoal = CustomActivity(this)
+        val tvPrimaryText = customMaxGoal.findViewById<TextView>(R.id.tvPrimaryText)
+        tvPrimaryText.text = "Maximum Goal"
+        goalContainer.addView(customMaxGoal)
+
+
+
+
+
+
     }
 }
